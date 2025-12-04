572755c751d3026f3130a9d0240802df4dc7d1a7
Jon <jonathan.martinez@maxikash.com>
2025-12-04 12:54:05 -0600

    Cambios

---
+++ backend/config/config.php
@@ -0,0 +1,5 @@
+<?php
+
+define('TOKEN', '__SPARTA_TOKEN_REDACTED__');  // tu token real
+
+define('ENDPOINT', 'https://servicios.s2movil.net/s2__SPARTA_SECRET_REDACTED__/estadocuenta'); // tu endpoint real
